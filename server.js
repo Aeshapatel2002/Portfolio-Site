0684c3268ea66c263f666dbfa7353722bd5bf871
Aeshapatel2002 <135282907+Aeshapatel2002@users.noreply.github.com>
2023-06-24 23:11:32 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const port = 3001;
 const path = require('path');
 const expressLayouts = require('express-ejs-layouts');
 const viewPath = path.join(__dirname, './views')
+const { MongoClient } = require('mongodb');
+const uri = 'mongodb://127.0.0.1:27017/'; 
+const client = new MongoClient(uri, { useUnifiedTopology: true });
+const bodyParser = require('body-parser');
+
     //view engine to EJS
 
 
@@ -49,8 +54,38 @@ app.get('/contact', function(req, res) {
     const title = 'Contact'; // Define the title variable
     res.render('contact', { title: title }); // Pass the title variable to the template
 });
+app.get('/login', function(req, res) {
+    const title = 'Login'; // Define the title variable
+    const errorMessage = 'Invalid username or password'; // Define the error message variable
+    res.render('login', { title: title, errorMessage: errorMessage }); // Pass the title and error message variables to the template
+  });
+  app.post('/login', function(req, res) {
+    const { username, password } = req.body;
+  
+    // Perform user authentication logic
+    if (username === 'admin' && password === 'password') {
+      // Authentication successful
+      res.redirect('/dashboard'); // Redirect to the dashboard or authorized area
+    } else {
+      // Authentication failed
+      res.render('login', { errorMessage: 'Invalid username or password' });
+    }
+  });
+  app.post('/login', function(req, res) {
+    // Handle the login logic here
+    // ...
+  });
+  
+  
+  app.get('/sign-up', function(req, res) {
+    res.render('sign-up'); // Render the signup view
+  });
 
+  app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+  
 // Start the server
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server listening on port ${port}`);
 });
+
